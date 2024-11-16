b8ea38a6becaa38a7bfcbf37938302c86da5691d
Aiden <amb@aspy.dev>
2024-11-15 19:42:45 -0500

    basic error handling for gemini

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -3,8 +3,13 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const geminiAPI = new GoogleGenerativeAI(process.env["GEMINI_KEY"]!);
 
 export async function askGemini(question: string) {
-	const reply = await geminiAPI
-		.getGenerativeModel({ model: "gemini-1.5-flash" })
-		.generateContent([question]);
-	return reply.response.text();
+	try {
+		const reply = await geminiAPI
+			.getGenerativeModel({ model: "gemini-1.5-flash" })
+			.generateContent([question]);
+		return reply.response.text();
+	} catch (error) {
+		console.error("Gemini Error!");
+		return "{Gemini failed to produce output}";
+	}
 }
