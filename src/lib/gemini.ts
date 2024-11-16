b40495aae9e5d9a3ab7e90ad1009596f3dfa6984
Aiden <amb@aspy.dev>
2024-11-15 19:58:00 -0500

    make better erorr

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -2,14 +2,24 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const geminiAPI = new GoogleGenerativeAI(process.env["GEMINI_KEY"]!);
 
+const timeout = new Promise<never>((_, reject) => {
+	setTimeout(() => {
+		reject(new Error("Promise timeout"));
+	}, 2000);
+});
+
 export async function askGemini(question: string) {
 	try {
-		const reply = await geminiAPI
-			.getGenerativeModel({ model: "gemini-1.5-flash" })
-			.generateContent([question]);
-		return reply.response.text();
+		return Promise.race([timeout, geminiAPIRequest(question)]);
 	} catch (error) {
 		console.error("Gemini Error!");
-		return "{Gemini failed to produce output}";
+		return "{Gemini failed to produce output [timeout]}";
 	}
+}
+
+async function geminiAPIRequest(question: string) {
+	const reply = await geminiAPI
+		.getGenerativeModel({ model: "gemini-1.5-flash" })
+		.generateContent([question]);
+	return reply.response.text();
 }
