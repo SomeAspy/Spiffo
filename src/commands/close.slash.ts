75cfdeae5373340d14a9611067a8034546574c22
Aiden <amb@aspy.dev>
2024-11-30 13:10:09 -0500

    close command

---
+++ src/commands/close.slash.ts
@@ -0,0 +1,18 @@
+import {
+	type ChatInputCommandInteraction,
+	PermissionFlagsBits,
+	SlashCommandBuilder,
+} from "discord.js";
+
+export const data = new SlashCommandBuilder()
+	.setName("closeThread")
+	.setDescription("close this thread")
+	.setDefaultMemberPermissions(PermissionFlagsBits.ManageThreads);
+
+export async function execute(interaction: ChatInputCommandInteraction) {
+	if (interaction.channel?.isThread()) {
+		interaction.reply(`Thread closed by ${interaction.user.displayName}`);
+		interaction.channel.setLocked(true);
+		interaction.channel.setArchived(true);
+	}
+}
